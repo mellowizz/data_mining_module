e9452473f4727b1a2c3be27900813aca3f2e6a22
T. Niklas Moran <moran@campus.tu-berlin.de>
2015-12-11 09:42:27 +0100

    sql_utilities now passes flake8!

--- sql_utilities.py
+++ sql_utilities.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """ Created on Wed Aug  5 22:45:08 2015
 
-@author: Moran """ import os from os.path import expanduser
+@author: Moran """
 
-import pandas.io.sql as psql from sqlalchemy import create_engine import sys
-import database.PostGISDatabase as PG from sklearn.cross_validation import
-train_test_split import pandas as pd
+import os
+from os.path import expanduser
+import pandas.io.sql as psql
+from sqlalchemy import create_engine
 
 try:
     from os import scandir
@@ -29,17 +30,20 @@ def testing(engine, csv_folder, parameter, table_name="test"):
                                                             parameter)
     mydf = psql.read_sql(sql_str, engine)
     mydf = mydf[mydf[parameter].isin(paramdict[parameter])]
+    a_df = mydf.select_dtypes(['float'])
     a_df.insert(0, parameter, mydf[parameter])
     # if "/" in a_df[parameter]:
     #    a_df[parameter] = a_df[parameter].replace(to_replace='/', value='-')
     # if " " in a_df[parameter]:
     #    a_df[parameter] = re.sub(r"[\\s]", "_", a_df[parameter])
     cols = list(a_df)
-    cols.insert(0, cols.pop(cols.index(parameter))) a_df = a_df.ix[:, cols]
+    cols.insert(0, cols.pop(cols.index(parameter)))
+    a_df = a_df.ix[:, cols]
     out_table = '_'.join(["train", parameter])
     a_df.to_sql(out_table, engine, if_exists='replace')
     file_path = os.path.join(csv_folder, "{0}.csv".format(out_table))
-    a_df.astype(object) a_df.astype(object).convert_objects()
+    a_df.astype(object)
+    a_df.astype(object).convert_objects()
     print(a_df.dtypes)
     print("writing file: {0}".format(file_path))
     a_df.to_csv(file_path, index=False, encoding='utf-8')
@@ -54,11 +58,11 @@ def training(engine, csv_folder, paramdict, parameter='all',
                   WHERE \"{1}\" IS NOT NULL or
                   \"{1}\" != '' ORDER BY \"{1}\" """.format(table_name,
                                                             parameter)
-    print(sql_str) mydf =
-    psql.read_sql(sql_str, engine)
+    print(sql_str)
+    mydf = psql.read_sql(sql_str, engine)
     if not parameter:
-        raise ValueError print
-        ("You must include a parameter! ")
+        raise ValueError
+        print ("You must include a parameter! ")
     else:
         mydf = mydf[mydf[parameter].isin(paramdict[parameter])]
     a_df = mydf.select_dtypes(['float'])
@@ -68,7 +72,8 @@ def training(engine, csv_folder, paramdict, parameter='all',
     #    a_df[parameter] = a_df[parameter].replace(to_replace='/', value='-')
     # if " " in a_df[parameter]:
     #    a_df[parameter] = re.sub(r"[\\s]", "_", a_df[parameter])
-    cols = list(a_df) cols.insert(0, cols.pop(cols.index(parameter)))
+    cols = list(a_df)
+    cols.insert(0, cols.pop(cols.index(parameter)))
     a_df = a_df.ix[:, cols]
     out_table = '_'.join([table_name, parameter])
     a_df.to_sql(out_table, engine, if_exists='replace')
@@ -80,15 +85,18 @@ def training(engine, csv_folder, paramdict, parameter='all',
     a_df = a_df.sample(n=20)
     a_df.to_csv(file_path, index=False, encoding='utf-8')
 
-def get_table_names(myfolder): """ returns list of table names from the csv
-files in given folder. """ return [fname.name.split('.csv')[0] for fname in
-                                   scandir(myfolder) if not
-                                   fname.name.startswith('.') and
-                                   fname.is_file()]
+
+def get_table_names(myfolder):
+    """ returns list of table names from the csv files in given folder. """
+    return [fname.name.split('.csv')[0]
+            for fname in scandir(myfolder)
+            if not fname.name.startswith('.')
+            and fname.is_file()]
 
 
 def get_csv_folder(csv_folder="csvDump"):
-    """" returns the csv folder where csv files should be dumped from database. """
+    """" returns the csv folder where csv
+    files should be dumped from database. """
     if os.name == 'nt':
         home = os.environ.get("USERPROFILE")
     else:
@@ -101,21 +109,20 @@ def get_csv_folder(csv_folder="csvDump"):
 
 if __name__ == '__main__':
     INPUT_TABLE = "test"
-    paramdict = { "natflo_wetness": ["dry", "mesic", "very wet"],
-                   "natflo_depression": ["0", "1"],
-                    "natflo_hydromorphic": ["0", "1"],
-                    "natflo_immature_soil": ["0", "1"],
-                    "natflo_species_richness": ["species_poor", "species_rich"],
-                    "natflo_usage": ["grazing", "mowing", "orchards",
-                                     "vineyards"],
-                    "natflo_usage_intensity": ["high", "medium", "low"],
-                    "eagle_vegetationtype": ["graminaceous_herbaceous",
-                                             "herbaceous", "shrub", "tree"]
-                }
+    paramdict = {"natflo_wetness": ["dry", "mesic", "very wet"],
+                 "natflo_depression": ["0", "1"],
+                 "natflo_hydromorphic": ["0", "1"],
+                 "natflo_immature_soil": ["0", "1"],
+                 "natflo_species_richness": ["species_poor", "species_rich"],
+                 "natflo_usage": ["grazing", "mowing", "orchards",
+                                  "vineyards"],
+                 "natflo_usage_intensity": ["high", "medium", "low"],
+                 "eagle_vegetationtype": ["graminaceous_herbaceous",
+                                          "herbaceous", "shrub", "tree"]}
     DSN = 'postgresql://postgres@localhost:5432/rlp_spatial'
     PARAM = "natflo_wetness"
     engine = create_engine(DSN)
     for param in paramdict:
         training(engine, os.path.join(os.getcwd(), 'data'), paramdict,
-                 parameter=param, table_name='test') 
-    #testing(engine, os.path.join(os.getcwd(), 'data'), PARAM, "test")
+                 parameter=param, table_name='test')
+    # testing(engine, os.path.join(os.getcwd(), 'data'), PARAM, "test")
