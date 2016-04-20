9e4bc98d18336f61028da1f961bfe84826acf1b4
Niklas Moran <moran@campus.tu-berlin.de>
2016-04-20 08:30:28 -0400

    working version

--- pipeline.py
+++ pipeline.py
@@ -7,6 +7,7 @@ import sys
 import io
 import os
 from sklearn import cross_validation
+from sklearn.cross_validation import cross_val_score
 from sklearn import metrics
 from sklearn import pipeline
 from sklearn.ensemble import ExtraTreesClassifier
@@ -15,6 +16,7 @@ import sklearn.tree as tree
 from evolutionary_search import EvolutionaryAlgorithmSearchCV
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectFromModel
 import pandas.io.sql as psql
@@ -37,7 +39,7 @@ class ColumnSelector(object):
         return self
 
 
-logging.basicConfig(filename='data_mining_module.log',
+logging.basicConfig(filename='pipeline.log',
                     format='%(asctime)s %(message)s',
                     level=logging.INFO)
 
@@ -46,24 +48,36 @@ DSN = 'postgresql://postgres@localhost:5432/rlp_saarburg'
 # load this from dict cursor?
 paramdict = {
     "natflo_wetness": ["dry", "mesic", "wet"],
-    "natflo_acidity": ["alkaline", "acid"]
+    "natflo_alkaline": ["0", "1"]
 }
 
-et_params = {'n_estimators': 400, 'random_state': 0, 'n_jobs': 4,
-             'class_weight': 'balanced'}
 
+def plot_top_features(atree, X, num_feat=10):
+    importances = atree.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in atree.estimators_],
+                 axis=0)
+    indices = np.argsort(importances)[::-1]
+    for f in range(num_feat):
+        logging.info(("{} feature {} ({})".format(f + 1, X.columns[indices[f]],
+                                                  importances[indices[f]])))
+    # Plot the feature importances of the forest
+    plt.figure()
+    plt.title("Feature importances")
+    plt.bar(range(10), importances[indices][:10],
+            color="r", yerr=std[indices][:10], align="center")
+    plt.xticks(range(10), X.columns[indices][:10])
+    plt.xlim([-1, 10])
+    plt.xlabel("Feature")
+    plt.show()
 
 def read_db_table(table, parameter='all'):
     engine = create_engine(DSN)
     with engine.connect():
         ''' read data from table'''
         datatable = psql.read_sql('''SELECT * FROM {}
-                                  --where natflo_wetness is not null
-                                  --and natflo_immature_soil is not null
-                                  --and natflo_species_richness is not null
-                                  --and natflo_acidity is not null
                                   '''.format(table),
                                   engine, index_col='id')
+
         if parameter == 'all':
             parameter = paramdict.keys()
         else:
@@ -73,34 +87,15 @@ def read_db_table(table, parameter='all'):
                       "we1min", "bodenart_kn1", "we2min"]:
                 datatable = datatable.drop(i, axis=1)
         except ValueError as e:
-            print(e.message)
+            logging.info(e.message)
 
-        datatable = datatable.fillna("unclass", axis=1)
-        y = pd.DataFrame(datatable[parameter])
+        datatable = datatable.fillna(0, axis=1)
+        # datatable = datatable.dropna()
+        # datatable.drop_duplicates()
+        y = pd.DataFrame(datatable[parameter]) #, dtype=str)
         X = datatable.drop(parameter, axis=1)
         X = X.select_dtypes(['float64'])
         return X, y
-
-
-def print_tree(t, root=0, depth=1):
-    if depth == 1:
-        print('def predict(X_i):')
-    indent = '    '*depth
-    print(indent + '# node %s: impurity = %.2f' %
-          (str(root), t.impurity[root]))
-    left_child = t.children_left[root]
-    right_child = t.children_right[root]
-
-    if left_child == tree._tree.TREE_LEAF:
-        print(indent + 'return %s # (node %d)' % (str(t.value[root]), root))
-    else:
-        print(indent + 'if X_i[%d] < %.2f: # (node %d)' % (t.feature[root],
-                                                           t.threshold[root],
-                                                           root))
-        print_tree(t, root=left_child, depth=depth+1)
-
-        print(indent + 'else:')
-        print_tree(t, root=right_child, depth=depth+1)
 
 
 def get_lineage(tree, feature_names, wet_classes,
@@ -123,14 +118,14 @@ def get_lineage(tree, feature_names, wet_classes,
             def recurse(left, right, child, lineage=None):
                 if lineage is None:
                     try:
-                        print(str(value[child]))
+                        # logging.info(str(value[child]))
                         lineage = [wet_classes[np.argmax(value[child])]]
                     except KeyError as f:
-                        print(f)
+                        logging.info(f)
                     except IndexError as f:
-                        print("{}: {}".format(f, wet_classes))
+                        logging.info("{}: {}".format(f, wet_classes))
                     except AttributeError as r:
-                        print("{}".format(r))
+                        logging.info("{}".format(r))
                 if child in left:
                     parent = np.where(left == child)[0].item()
                     split = '<='
@@ -157,7 +152,7 @@ def get_lineage(tree, feature_names, wet_classes,
                         if type(node) == tuple:
                             if not (isinstance(node[2], float) and
                                     isinstance(node[3], int)):
-                                print("skipping tuple..")
+                                logging.info("skipping tuple..")
                                 continue
                             a_feature, a_split, a_threshold, a_parent_node = node
                             tree_csv.write("{},{},{:5f},{}\n".format(a_feature,
@@ -166,30 +161,42 @@ def get_lineage(tree, feature_names, wet_classes,
                                                                      a_parent_node,
                                                                     ))
                         else:
-                            print(node)
                             tree_csv.write(''.join([node, "\n"]))
                 except TypeError as e:
-                    print(e)
+                    logging.info(e)
     except ValueError as e:
-        print(e)
+        logging.info(e)
     except KeyError as f:
-        print(f)
+        logging.info(f)
     except UnboundLocalError as e:
-        print(e.message)
+        logging.info(e.message)
 
 if __name__ == '__main__':
     table = sys.argv[1]
     X, y = read_db_table(table)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4,
-                                                        stratify=y)
-
+    ''' write test data '''
+    engine = create_engine(DSN)
     report_folder = os.path.join(os.getcwd(), "training_cm")
     rules_folder = os.path.join(os.getcwd(), "rules")
     for curr in paramdict:
-        y_train_curr = y_train[curr]
-        y_test_curr = y_test[curr]
-
-        parameters = {
+        y_curr = y[curr]
+        msk = np.random.rand(len(X)) < 0.6
+        y_train, y_test = y_curr[msk], y_curr[~msk]
+        X_train, X_test = X[msk], X[~msk]
+        y_train_curr = y_train
+        y_test_curr = y_test
+        print("X: {}, y: {}".format(len(X), len(y)))
+        print("X_train: {}, y_train: {}".format(len(X_train), len(y_train)))
+        print("X_test: {}, y_test: {}".format(len(X_test), len(y_test)))
+        print("curr parameter: {}".format(curr))
+        if curr == 'wetness':
+            with engine.connect():
+                test = pd.concat([X_test, y_test], axis=1,
+                                join_axes=[X_test.index]).drop_duplicates()
+                test.index.name = 'id'
+                test_table = '_'.join(["test", table])
+                test.to_sql(test_table, engine, if_exists='replace')
+        dt_params = {
             'max_features': ['auto', 'sqrt', 'log2'],
             'max_depth': range(2, 20, 2),
             'criterion': ['gini', 'entropy'],
@@ -197,6 +204,10 @@ if __name__ == '__main__':
             'min_samples_leaf': range(2, 20, 2),
             'class_weight': ['balanced']
         }
+
+        et_params = {'n_estimators': 600,
+                    'class_weight': 'balanced'
+                }
         grid = {
             'dt__max_features': ['auto', 'sqrt', 'log2'],
             'dt__max_depth': range(2, 12, 2),
@@ -208,14 +219,25 @@ if __name__ == '__main__':
             }
 
         steps = [('feature_selection',
-                  #SelectKBest()),
-                  SelectFromModel(estimator=ExtraTreesClassifier(**et_params))),
-                 ('dt', DecisionTreeClassifier())]
+                #SelectKBest()),
+                SelectFromModel(estimator=ExtraTreesClassifier(**et_params))),
+                ('dt', DecisionTreeClassifier())]
         pipe = pipeline.Pipeline(steps)
 
-        cv = EvolutionaryAlgorithmSearchCV(
-            estimator=DecisionTreeClassifier(),  # pipe,
-            params=parameters,  # grid,
+        logging.info("*** ET *** ")
+        logging.info("{}".format(curr))
+        etree = ExtraTreesClassifier(**et_params)
+        etree.fit(X_train, y_train_curr)
+        # score = ross_val_score(etree, X_train, y_train_curr)
+        # logging.info("Training cross validation score: {}".format(score))
+        plot_top_features(etree, X_train)
+        y_pred = etree.predict(X_test)
+        report = metrics.classification_report(y_test_curr, y_pred)
+        # save report
+        logging.info(report)
+        dt = EvolutionaryAlgorithmSearchCV(
+            estimator=DecisionTreeClassifier(),
+            params=dt_params,
             scoring="accuracy",
             cv=StratifiedKFold(y_train_curr),
             verbose=1,
@@ -224,64 +246,29 @@ if __name__ == '__main__':
             tournament_size=3,
             generations_number=10,
             n_jobs=4)
-        cv.fit(X_train, y_train_curr)
-        y_pred = cv.predict(X_test)
-        report = metrics.classification_report(y_test_curr, y_pred)
-        print(report)
-        mydt = cv.best_params_
-        dt = DecisionTreeClassifier(**mydt).fit(X_train, y_train_curr)
+        dt.fit(X_train, y_train_curr)
         y_pred = dt.predict(X_test)
         report = metrics.classification_report(y_test_curr, y_pred)
-        #print("best estimator: {}".format(cv.best_estimator_))
-        #print("named step: {}".format(pipe.named_steps['dt']))
-        #print("best params: {}".format(cv.best_params_))
+        logging.info("*** DT *** ")
+        # save report
+        logging.info(report)
+        dt = DecisionTreeClassifier(**dt.best_params_).fit(X_train, y_train_curr)
+        y_pred = dt.predict(X_test)
+        report = metrics.classification_report(y_test_curr, y_pred)
+        logging.info(report)
         my_out_file = ''.join([rules_folder, '/', curr, '.csv'])
         get_lineage(dt, X_train.columns, paramdict[curr],
                     output_file=my_out_file)
-
-        report = metrics.classification_report(y_test_curr, y_pred)
-        # X_new =SelectKBest(k=pipe.named_steps['feature_selection']).fit_transform(X_train,
-        #                                                                    y_train_curr)
-        # mydt = DecisionTreeClassifier(cv.best_params_).fit(X_new, y_train_curr)
+        logging.info("DT fitted with best parameters")
+        # save report
+        logging.info(metrics.classification_report(y_test_curr, y_pred))
         with open(curr + '.dot', 'w+') as f:
             f = tree.export_graphviz(dt, out_file=f,
-                                     feature_names=X_train.columns,
-                                     class_names=paramdict[curr],
-                                     filled=True,
-                                     rounded=True,
-                                     special_characters=False)
+                                    feature_names=X_train.columns,
+                                    class_names=paramdict[curr],
+                                    filled=True,
+                                    rounded=True,
+                                    special_characters=False)
 
         subprocess.call(["dot", "-Tpng", curr + '.dot', "-o",
                             ''.join([report_folder, '/', curr, '.png'])])
-    ''' write test data '''
-    engine = create_engine(DSN)
-    with engine.connect():
-        test = pd.concat([X_test, y_test], axis=1)
-        test.index.name = 'id'
-        test_table = '_'.join([table, "test"])
-        test.to_sql(test_table, engine, if_exists='replace', index=True)
-        '''
-        features = cv.steps[-1]
-        print(X.columns[features.transform(np.arange(len(X.columns)))])
-        # feature_importances_
-        importances = pipe.named_steps['feature_selection'].feature_importances_
-        std = np.std([tree.feature_importances_ for tree in dt.estimators_],
-                    axis=0)
-        indices = np.argsort(importances)[::-1]
-        # Print the feature ranking
-        logging.info("Feature ranking:")
-        for f in range(num_feat):
-            logging.info(("{} feature {} ({})".format(f + 1, X.columns[indices[f]],
-                                                    importances[indices[f]])))
-
-        # Plot the feature importances of the forest
-        plt.figure()
-        plt.title("Feature importances")
-        plt.bar(range(10), importances[indices][:10],
-                color="r", yerr=std[indices][:10], align="center")
-        plt.xticks(range(10), X.columns[indices][:10])
-        plt.xlim([-1, 10])
-        plt.xlabel("Feature")
-        plt.show()
-        print(X.columns[indices][:10])
-        '''
